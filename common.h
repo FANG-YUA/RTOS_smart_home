7d98582c016e6ddc83926dd877cc71eed9be8684
FANG-YUA <2801245636@QQ.COM>
2025-08-18 14:58:42 +0800

    Create common.h

---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define FAN_CTRL_OFF_STATUS (0)
+#define FAN_CTRL_ON_STATUS (1)
+
+#define LIGHT_CTRL_OFF_STATUS (0)
+#define LIGHT_CTRL_ON_STATUS (1)
+
+
+#endif
